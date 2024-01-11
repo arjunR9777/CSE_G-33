f2dfab0d473c48a51e072a8fe890ebc692ad013e
arjunR9777 <149650287+arjunR9777@users.noreply.github.com>
2024-01-11 18:14:58 +0530

    Create App.js

---
+++ Frontend/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Header from './components/Header.js';
+import Home from './components/Home.js';
+import Footer from './components/Footer';
+
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+    <Home/>
+    </div>
+  );
+}
+
+export default App;
